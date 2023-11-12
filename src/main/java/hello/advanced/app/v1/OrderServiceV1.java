b78576b1c84878e4638eeee45be285b22fa64adf
BrantKijin <upper0house@gmail.com>
2023-11-12 23:58:23 +0900

    `1`

---
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,16 @@
+package hello.advanced.app.v1;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+	private final OrderRepositoryV1 orderRepository;
+
+	public void orderItem(String itemId){
+		orderRepository.save(itemId);
+	}
+}
