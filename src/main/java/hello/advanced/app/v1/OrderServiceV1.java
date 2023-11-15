4bfe19451f9661a3c6326917c0520ff228e5c2e8
곽기진 <upper0house@gmail.com>
2023-11-15 17:08:01 +0900

    1

--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -2,6 +2,8 @@ package hello.advanced.app.v1;
 
 import org.springframework.stereotype.Service;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -9,8 +11,18 @@ import lombok.RequiredArgsConstructor;
 public class OrderServiceV1 {
 
 	private final OrderRepositoryV1 orderRepository;
+	private final HelloTraceV1 trace;
 
 	public void orderItem(String itemId){
-		orderRepository.save(itemId);
+		TraceStatus status = null;
+		try{
+			status = trace.begin("OrderService.orderItem()");
+			orderRepository.save(itemId);
+			trace.end(status);
+		}catch (Exception e){
+			trace.exception(status,e);
+			throw e;
+		}
+
 	}
 }
